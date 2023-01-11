7dfac2265a588f6cbac6e5e23664335baec630c6
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-11 17:18:59 -0500

    Use a better import method

---
+++ 6-better-impot.js
@@ -0,0 +1,25 @@
+
+// better import nav-bar
+// SSR: server side rendering
+// moved index.html into public with its assets
+// note index.html in public is root and loaded with everything it needs on start
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('./public'))
+
+app.all('*',(req,res)=>{
+    res.status(404).send('resource not found')
+})
+
+app.listen(5000,() =>{
+    console.log('Listening to port 5000...')
+})
+
+
+/**
+ * API - json     SSR-  template
+ * Send data    send template
+ * Res.JSON()   RES.Render()
+ */
